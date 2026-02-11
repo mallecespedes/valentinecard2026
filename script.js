4258dae1936badd0ddcf3d5a4d12cc71f1af30db
Marielle Céspedes <mariellecespedes1@gmail.com>
2026-02-11 16:05:01 -0600

    Created script.js
    Adding logic and rules

---
+++ script.js
@@ -0,0 +1,38 @@
+const noBtn = document.getElementById("noBtn");
+const yesBtn = document.getElementById("yesBtn");
+
+let yesScale = 1;
+let chaosLevel = 0;
+
+noBtn.addEventListener("mouseenter", () => {
+  chaosLevel++;
+
+  const card = document.querySelector(".card");
+  const cardRect = card.getBoundingClientRect();
+
+  const maxX = cardRect.width - noBtn.offsetWidth;
+  const maxY = cardRect.height - noBtn.offsetHeight;
+
+  noBtn.style.left = Math.random() * maxX + "px";
+  noBtn.style.top = Math.random() * maxY + "px";
+
+  // YES grows
+  yesScale += 0.18;
+  yesBtn.style.transform = `scale(${yesScale})`;
+
+  // NO shrinks & rotates
+  const shrink = Math.max(0.4, 1 - chaosLevel * 0.08);
+  const rotation = Math.random() * chaosLevel * 15 - chaosLevel * 7;
+
+  noBtn.style.transform = `scale(${shrink}) rotate(${rotation}deg)`;
+
+  if (chaosLevel > 2) noBtn.classList.add("shake");
+  if (chaosLevel > 4) noBtn.classList.add("panic");
+
+  if (chaosLevel > 6) noBtn.innerText = "pls stop 😭";
+  if (chaosLevel > 8) noBtn.style.display = "none";
+});
+
+yesBtn.addEventListener("click", () => {
+  window.location.href = "success.html";
+});
